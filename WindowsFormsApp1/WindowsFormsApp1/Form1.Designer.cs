a75c8e7a71c71f21e51ce2d202414fa5d17aa07c
Андрей Маркин <andreymiw@yahoo.com>
2018-03-06 01:11:48 +0300

    New form by Andrey

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -1,16 +1,16 @@
-﻿namespace WindowsFormsApp1
+﻿namespace WindowsFormsApp2
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -20,171 +20,118 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Key = new System.Windows.Forms.TextBox();
+            this.Input = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
+            this.Output = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.label4 = new System.Windows.Forms.Label();
-            this.textBox4 = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // textBox1
+            // Key
             // 
-            this.textBox1.Location = new System.Drawing.Point(100, 71);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(372, 20);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.Key.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Key.Location = new System.Drawing.Point(12, 35);
+            this.Key.Multiline = true;
+            this.Key.Name = "Key";
+            this.Key.Size = new System.Drawing.Size(400, 90);
+            this.Key.TabIndex = 0;
             // 
-            // textBox2
+            // Input
             // 
-            this.textBox2.Location = new System.Drawing.Point(100, 112);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(682, 20);
-            this.textBox2.TabIndex = 1;
-            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(100, 196);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(682, 20);
-            this.textBox3.TabIndex = 2;
-            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(23, 74);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
-            this.label1.TabIndex = 3;
-            this.label1.Text = "Ключ";
+            this.Input.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Input.Location = new System.Drawing.Point(12, 177);
+            this.Input.Multiline = true;
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(400, 380);
+            this.Input.TabIndex = 3;
             // 
             // label2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 115);
+            this.label2.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label2.Location = new System.Drawing.Point(98, 9);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(48, 13);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Текст in";
+            this.label2.Size = new System.Drawing.Size(229, 23);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ключ";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Output
+            // 
+            this.Output.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
+            this.Output.Location = new System.Drawing.Point(561, 177);
+            this.Output.Multiline = true;
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(400, 380);
+            this.Output.TabIndex = 7;
             // 
             // label3
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(23, 199);
+            this.label3.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label3.Location = new System.Drawing.Point(98, 151);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(55, 13);
-            this.label3.TabIndex = 5;
-            this.label3.Text = "Текст out";
+            this.label3.Size = new System.Drawing.Size(229, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Ввод";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.DodgerBlue;
+            this.label1.Location = new System.Drawing.Point(647, 151);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(229, 23);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Вывод";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(100, 155);
+            this.button1.BackColor = System.Drawing.Color.PaleGreen;
+            this.button1.ForeColor = System.Drawing.Color.Black;
+            this.button1.Location = new System.Drawing.Point(438, 334);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(148, 23);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Зашифровать";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Size = new System.Drawing.Size(100, 67);
+            this.button1.TabIndex = 10;
+            this.button1.Text = "Конвертировать";
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(100, 260);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(148, 23);
-            this.button2.TabIndex = 7;
-            this.button2.Text = "Расшифровать";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(23, 336);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(40, 13);
-            this.label4.TabIndex = 8;
-            this.label4.Text = "Текст ";
-            // 
-            // textBox4
-            // 
-            this.textBox4.Location = new System.Drawing.Point(100, 333);
-            this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(682, 20);
-            this.textBox4.TabIndex = 9;
-            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.ForeColor = System.Drawing.Color.Red;
-            this.label5.Location = new System.Drawing.Point(387, 180);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(0, 13);
-            this.label5.TabIndex = 10;
-            // 
-            // richTextBox1
-            // 
-            this.richTextBox1.Location = new System.Drawing.Point(100, 385);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(682, 376);
-            this.richTextBox1.TabIndex = 11;
-            this.richTextBox1.Text = "";
-            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.ForeColor = System.Drawing.Color.OrangeRed;
-            this.label6.Location = new System.Drawing.Point(366, 9);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(227, 37);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "Новый дизайн";
-            this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.ClientSize = new System.Drawing.Size(1041, 804);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.textBox4);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.button2);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.SandyBrown;
+            this.ClientSize = new System.Drawing.Size(973, 582);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.Key);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Opacity = 0.95D;
+            this.Text = "Cool Шифрацкер";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -192,19 +139,13 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Key;
+        private System.Windows.Forms.TextBox Input;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Output;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.Label label6;
     }
 }
 
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form2.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form2 : Form
+    {
+        private string KeyToEncode;
+        private string enc = "";
+        string asci = "ABCDEFGHIJKLMNOPQRSTUVWXYZ АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ.,abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+|`1234567890-=/абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
+       
+        public string Decode1(string TxtIn, string Key)
+        {
+            string outtext = "";
+            for(int i=0;i<TxtIn.Length ;i++)
+            {
+                int x = enc.IndexOf(TxtIn[i]);
+                outtext += asci[x];
+            }
+            return outtext;
+        }
+        //-------------------------------------------------------------------------------------------------------------------------------------
+        public string Encode1(string TxtIn, string Key)
+        {
+            string outtext = "";
+            int j = 0,k = 0;
+            for(j=0; j<Key.Length;j++)              //Заполнение массива
+            {
+                if (enc.IndexOf(Key[j]) == -1)      //Проверка на повторы
+                {
+                    enc += Key[j];
+                }
+            }
+            while (k < asci.Length)                 //Заполнение массива
+            {
+                if (enc.IndexOf(asci[k]) == -1)     //Проверка на повторы
+                {
+                    enc += asci[k];
+                }
+                k++;
+            }
+            richTextBox1.AppendText(enc);
+            for(int i=0;i<TxtIn.Length; i++)
+            {
+               int x = asci.IndexOf(TxtIn[i]);    //System.Text.Encoding.Unicode.GetBytes(input)
+                outtext += enc[x];
+            }
+            richTextBox1.AppendText(" ");
+            return outtext;
+            /*
+             Самый простой конвертер на основе ASCII кодов.Каждому символу соотв.ascii код -функция преобразования Char To Byte. То есть символ можно перевести в число
+             1.Сформировать таблицу для перекодирования
+             Это массив, который надо заполнить.
+             Можно сначала заполнить элементами ключа - по одному символу, а потом оставшимися символами.При этом надо каждый раз смотреть, чтобы не было повторов
+             самое главное, чтобы символы в таблице перекодировки были все разные, иначе обратно дешифровать не получится
+             2.Сохранить куда - нить этот массив для расшифровки
+             3.Берем по одному символу из Входящей строки(например английское "а") преобразуем его в число = ascii код(будет 65)
+             4.Берем соответствующий элемент из таблицы перекодировки(65 элемент массива) и записываем его в выходную строчку
+             5.так делаем с каждым символом из txtin
+             */
+        }
+        //----------------------------------------------------------------------------------------------------------------------------------------------//
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        //----------------------------------------------------------------------------------------------------------------------------------------------//
+        private void button1_Click(object sender, EventArgs e)
+        {
+            KeyToEncode = textBox1.Text;                            // берем ключ из текстбокса 1
+            string Textin = textBox2.Text;                          // а текст для зашифровки из текстбокса 2
+            if (KeyToEncode == "")
+            {                                // если ключ пустой, то выдать ошибку
+                label5.Text = "Error: Key should not be empty";
+                return;                                             // и выйти из процедуры
+            }
+            if (Textin == "")
+            {                                                       // если строка для шифрования пустая, то выдать ошибку
+                label5.Text = "Error: Text to encode should not be empty";
+                return;                                             // и выйти из процедуры
+            }
+            label5.Text = "";                                       // стираем ошибки
+            textBox3.Text = Encode1(Textin, KeyToEncode);            // вызываем конвертер
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox4.Text = Decode1(textBox3.Text, textBox1.Text);
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
